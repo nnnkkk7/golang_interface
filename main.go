f397ec37bb50bb6e2de5c8f7c90f62d34c7875d3
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-01-24 21:24:06 +0900

    use interface

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+)
+
+//インターフェースの実装
 type shape interface {
-	area() float64
+	getArea() float64
+}
+
+func outputArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+type rectangle struct {
+	tate float64
+	yoko float64
+}
+
+type triangle struct {
+	takasa float64
+	teihen float64
+}
+
+func (r *rectangle) getArea() float64 {
+	return r.tate * r.yoko
+}
+
+func (t *triangle) getArea() float64 {
+	return t.teihen * t.takasa / 2
+}
+
+func main() {
+	t := &rectangle{tate: 2, yoko: 5}
+	r := &triangle{takasa: 9, teihen: 4}
+
+	//四角形と三角形の面積
+	outputArea(t)
+	outputArea(r)
 }
